9978e44e943cd167a186c0abb8658012f63e81f4
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-08-22 18:48:57 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let response = '';
+  for (let i = string.length; i >= 0 ; i--) {
+    response += string.slice(i, i + 1);
+  }
+  return response;
 };
 
 // Do not edit below this line
