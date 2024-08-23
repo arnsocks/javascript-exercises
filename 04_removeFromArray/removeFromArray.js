e9035127a2ad3ed15dbbd5aef28293784e4c2757
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-08-23 17:38:41 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+  for (const arg of args) { //look at every removal argument...
+    for (let i = array.length -1; i >= 0; i--) { // and every item still in the array
+      if (arg === array[i]) { // splice them out if they match
+        array.splice(i, 1);       
+      }
+    }
+  }
+
+  /* This is the technique used in the provided solution 
+  let newArray = [];
+  for (const item of array)
+  {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+  */
+
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
