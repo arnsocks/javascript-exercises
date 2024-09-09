265ef2062c030d2b128ab29d30f3ed7262385e19
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-09-09 10:54:59 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+ 
+  switch(true) {
+    case n < 0:
+      return "OOPS";
+    case n == 0:
+      return 0;
+    case n == 1:
+      return 1;
+    default:
+      return (fibonacci(n - 1) + fibonacci(n - 2));
+  }
 };
 
 // Do not edit below this line
