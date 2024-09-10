af0fb80b03d0619720ec59affab12bdfc72a8ec3
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-09-10 17:35:19 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((oldestFound, current) => {
+    return findAge(oldestFound) >= findAge(current) ? oldestFound : current; 
+  })
 };
 
+const findAge = function(person) {
+  if (!person.yearOfDeath) {
+    person.yearOfDeath = new Date().getFullYear();
+  }
+  let age = person.yearOfDeath - person.yearOfBirth;
+  return age;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
